c4646b78706a4851ef5e3c6f95ae0a38effa3a37
joy <wendokingoo@gmail.com>
2019-02-27 16:02:19 +0300

    photos html file added

--- photogallery/views.py
+++ photogallery/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render
 
 from django.http  import HttpResponse
 
+import datetime as dt
+
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to Photo Gallery')
+
+def photo(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
